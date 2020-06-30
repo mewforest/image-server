838f452519e62b4f603370f6fdc0278a24bb16fe
Mew Forest <mew.forest@gmail.com>
2020-06-30 22:05:36 +0300

    Добавил фронтенд

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for, request, render_template
 import os
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/json/images')
 def json_images():
-    original = os.listdir('static/images/original')
-    thumbs = os.listdir('static/images/thumbs')
-    return jsonify([original, thumbs])
+    json = []
+    image_names = os.listdir('static/images/original')
+    for index, name in enumerate(image_names):
+        json.append({
+            'id': index,
+            'title': name.replace('.jpg', ''),
+            'url': request.url_root + url_for('static', filename=f'images/original/{name}'),
+            'thumbnailUrl': request.url_root + url_for('static', filename=f'images/thumbs/{name}'),
+        })
+    return jsonify(json)
 
 
 if __name__ == '__main__':
